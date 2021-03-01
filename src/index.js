6984f9198dec7b3168bfa74726e44932d7ac3faa
Dongseon Lee <eastline@maycan.co.kr>
2021-03-01 20:42:22 +0900

    #1.3 Subscriptions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import {createStore} from "redux";
 
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+number.innerText = 0;
+
 const countModifier = (count = 0, action) => {
   if(action.type === "ADD"){
     return count + 1;
@@ -9,13 +15,21 @@ const countModifier = (count = 0, action) => {
     return count;
   }
 };
-
 const countStore = createStore(countModifier);
 
-countStore.dispatch({type: "ADD"});
-countStore.dispatch({type: "ADD"});
-countStore.dispatch({type: "ADD"});
-countStore.dispatch({type: "ADD"});
-countStore.dispatch({type: "ADD"});
-countStore.dispatch({type: "MINUS"});
-console.log(countStore.getState());
+const onChange = () => {
+  number.innerText = countStore.getState();
+}
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({type: "ADD"});
+};
+
+const handleMinus = () => {
+  countStore.dispatch({type: "MINUS"});
+};
+
+add.addEventListener("click", handleAdd)
+minus.addEventListener("click", handleMinus)
